9713139db2756030a32b79475747ee7fc78da370
oxmox <martishkaoxanka@gmail.com>
2022-10-28 05:32:07 +1000

    2/6

--- JSON - XML converter/task/src/converter/Main.java
+++ JSON - XML converter/task/src/converter/Main.java
@@ -1,44 +1,140 @@
 package converter;
 
-import java.util.Scanner;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
-        var str = new Scanner(System.in)
-                            .nextLine()
-                            .trim();
-        if (str.startsWith("<")) {
-            var result = str
-                              .replaceAll("[<>/]", " ")
-                              .trim()
-                              .split(" ");
-            System.out.println("{" +
-                    "\"" +
-                    result[0] +
-                    "\": " +
-                    (result.length == 1
-                            ?
-                            null + "}"
-                            :
-                            "\"" + result[1] + "\"}"));
-
-        } else if (str.startsWith("{")) {
-            var result = str
-                              .replaceAll("[{}\" ]", "")
-                              .trim()
-                              .split(":");
-
-            if (!"null".equals(result[1])) {
-                System.out.printf(
-                        "<%s>%s</%s>",
-                        result[0],
-                        result[1],
-                        result[0]);
-            } else {
-                System.out.printf("<%s/>", result[0]);
-            }
+        var path = "test.txt";
+        var str = getData(path);
+        if (Objects.requireNonNull(str).startsWith("<")) {
+            xmlToJson(str);
         } else {
-            System.out.println("Unknown data format");
+            jsonToXml(str);
+        }
+    }
+
+    private static String getData(String path) {
+        try (var bReader = new BufferedReader(
+                           new FileReader(path)))
+        {
+            var sb = new StringBuilder();
+            while (bReader.ready()) {
+                sb.append(bReader.readLine());
+            }
+            return sb.toString();
+
+        } catch (Exception e) {
+            System.out.println("Something went wrong!");
+            return null;
+        }
+    }
+
+    private static void xmlToJson(String str) {
+        String tag;
+        String value;
+        List<String> attributes = new ArrayList<>();
+        var tagM = Pattern
+                                .compile("<[\\w/.]+")
+                                .matcher(str);
+        var valueM = Pattern
+                                     .compile(">[\\w\\s.]+<")
+                                    .matcher(str);
+        var attributeM = Pattern
+                                       .compile("\\w+ = \"\\w+\"")
+                                      .matcher(str);
+
+        tag = tagM.find() ?
+                tagM
+                        .group()
+                        .replaceAll("[</>]", "")
+                : "";
+        value = valueM.find() ?
+                valueM
+                        .group()
+                        .replaceAll("[<>]", "\"")
+                : null;
+        while (attributeM.find()) {
+            attributes.add(attributeM.group());
+        }
+
+        if (attributes.isEmpty()) {
+            if (Objects.requireNonNull(value).isEmpty()) {
+                System.out.printf(
+                        "{\"%s\" : %s}%n", tag, null);
+            } else {
+                System.out.printf(
+                        "{\"%s\" : \"%s\"}%n", tag, value);
+            }
+
+        } else {
+            var attr = attributes
+                    .stream()
+                    .map(x ->
+                            "\t\t\"@" +
+                            x.replaceAll(" =", "\" :") +
+                            ",\n")
+                    .reduce(String::concat).get();
+            var result = String.format(
+                    "{\n \t\"%s\" : \n\t{\n %s \t\t\"#%s\" : %s\n\t}\n}",
+                    tag,
+                    attr,
+                    tag,
+                    value);
+            System.out.println(result);
+        }
+    }
+    private static void jsonToXml(String str) {
+        String tag;
+        String value;
+        List<String> attributes = new ArrayList<>();
+        var tagJ = Pattern
+                                 .compile("\"[\\w.]+\"")
+                                 .matcher(str);
+        var valueJ = Pattern
+                                 .compile("\"#[\\w.]+\" : (?!null)[\"\\w\\s]+")
+                                 .matcher(str);
+        var attrJ = Pattern
+                                       .compile("\"@[\\w.]+\" : [\"\\w]+")
+                                       .matcher(str);
+        while (attrJ.find()) {
+            attributes.add(attrJ.group());
+        }
+        tag = tagJ.find() ?
+                tagJ
+                        .group()
+                        .replaceAll("\"", "")
+                : "";
+        value = valueJ.find() ?
+                valueJ
+                        .group()
+                        .replaceAll("\"#[\\w.]+\" : ", "")
+                : "";
+        var attr = attributes
+                .stream()
+                .map(x -> (x.matches(".+\\d+") ?
+                        (x + "\"")
+                        : x))
+                .map(x -> x.replaceAll("\"@", " "))
+                .map(x -> x.replaceAll("\" : \"?", " = \""))
+                .reduce(String::concat)
+                .orElse("");
+
+        if (value.isEmpty()) {
+            System.out.printf("<%s%s/>%n", tag, attr);
+
+        } else {
+            value = value.replaceAll("\"", "");
+            System.out.printf("<%s%s>%s</%s>%n",
+                    tag,
+                    attr,
+                    value,
+                    tag);
         }
     }
 }
